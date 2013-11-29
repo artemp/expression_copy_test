3ad86185a0249386b3c6ebb80e8836f9e85b1535
artemp <artem@mapnik.org>
2013-11-29 16:12:47 +0000

    improve test case

--- copy_map_test.cpp
+++ copy_map_test.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 // mapnik
 #include <mapnik/load_map.hpp>
+#include <mapnik/save_map.hpp>
 #include <mapnik/config_error.hpp>
 #include <mapnik/datasource_cache.hpp>
 #include <mapnik/font_engine_freetype.hpp>
+#include <mapnik/value.hpp>
+#include <mapnik/expression_node_types.hpp>
+#include <mapnik/expression_string.hpp>
+#include <mapnik/symbolizer.hpp>
+#include <mapnik/enumeration.hpp>
+#include <mapnik/image_compositing.hpp>
+
 // boost
 #include <boost/timer/timer.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/find.hpp>
+#include <boost/mpl/at.hpp>
+
+#include <cassert>
 
 int main(int argc, char** argv)
 {
@@ -33,13 +46,19 @@ int main(int argc, char** argv)
         std::cerr << ex.what() << std::endl;
     }
 
-
     {
         std::cerr << "copying map " << NUM_RUNS << " times.. " << std::endl;
         boost::timer::auto_cpu_timer t;
         for (int i=0; i< NUM_RUNS; ++i)
         {
             mapnik::Map new_map(m); // copy ctor
+            if (i == 0)
+            {
+                if (mapnik::save_map_to_string(m) != mapnik::save_map_to_string(new_map))
+                {
+                    throw std::runtime_error("FAIL");
+                }
+            }
         }
     }
 
@@ -48,10 +67,12 @@ int main(int argc, char** argv)
         boost::timer::auto_cpu_timer t;
         for (int i=0; i< NUM_RUNS; ++i)
         {
-            mapnik::load_map(m, argv[1]);
+            mapnik::Map new_map(256,256);
+            mapnik::load_map(new_map, argv[1]);
         }
     }
 
     std::cerr << "Done" << std::endl;
+
     return 0;
 }
